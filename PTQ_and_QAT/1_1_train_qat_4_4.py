3a72fcabbd59cfa407ba1bab87dc2d660acf2db2
Nicolò Ghielmetti <nicolo.ghielmetti@gmail.com>
2023-11-05 00:30:52 +0100

    added plot of loss during training

--- PTQ_and_QAT/1_1_train_qat_4_4.py
+++ PTQ_and_QAT/1_1_train_qat_4_4.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pickle
 from datetime import datetime
 from json import JSONEncoder
 
@@ -7,6 +8,7 @@ import keras.models
 import numpy as np
 import qkeras.utils
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
+from matplotlib import rc, pyplot as plt
 
 from quantized_float import quantized_float, quantized_float_softmax, quantized_float_sigmoid, quantized_float_tanh
 from shared_definitions import SharedDefinitons
@@ -62,6 +64,22 @@ history = quantized_model.fit(X_train, y_train, batch_size=512, epochs=50,
 
 test_perf = quantized_model.evaluate(x=X_test, y=y_test, verbose=1, workers=12, use_multiprocessing=True,
                                      return_dict=True, callbacks=[mc, es, tb])
+
+with open(model_name + '/' + model_name + '_train_val_loss_history.pkl', 'wb') as file_pi:
+    pickle.dump(history.history, file_pi)
+
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
+plt.figure(figsize=(16, 9))
+plt.plot(history.history['loss'], linewidth=1)
+plt.plot(history.history['val_loss'], linewidth=1)
+plt.title('Model Loss over Epochs for model: ' + model_name)
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Training Sample Loss', 'Validation Sample Loss'])
+plt.savefig(model_name + '_train_val_loss_plot.pdf', dpi=500)
+plt.close()
+
 with open(model_name + '/' + model_name + '_test_performance.json',
           'w') as json_file:
     json.dump(test_perf, json_file)
